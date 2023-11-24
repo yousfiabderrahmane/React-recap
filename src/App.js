0f00f10169a0465ed1038b8c184b2ab4f7161cad
yousfiabderrahmane <bdrrhmnyousfi@gmail.com>
2023-11-24 15:19:53 +0100

    feat:  arrow func. + ternary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [addBy, setAddBy] = useState(0);
+  const [showText, setShowText] = useState(false);
+
+  const DoSomething = (num) => {
+    setNumber((number) => number + num);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {showText ? (
+          <>
+            <h1>You will add by {addBy}</h1>
+            <h1>{number}</h1>
+            <button onClick={() => DoSomething(addBy)}>Add</button>
+          </>
+        ) : (
+          <>
+            <h1>Enter a Number</h1>
+            <input
+              type="text"
+              onChange={(e) => setAddBy(Number(e.target.value))}
+            />
+            <button onClick={() => (addBy > 0 ? setShowText(true) : "")}>
+              OK
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
