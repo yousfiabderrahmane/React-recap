88ebbd5bae456e3020450e1d8607ea5827f0bcf1
yousfiabderrahmane <bdrrhmnyousfi@gmail.com>
2023-11-24 15:32:57 +0100

    feat: litle cute app

--- src/App.js
+++ src/App.js
@@ -2,36 +2,33 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const [addBy, setAddBy] = useState(0);
-  const [showText, setShowText] = useState(false);
+  const [id, SetId] = useState(0);
+  const [todo, setTodo] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const DoSomething = (num) => {
-    setNumber((number) => number + num);
+  const fetchToDoById = async (id) => {
+    setLoading(true);
+    setError(null);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    if (res.ok) {
+      const data = await res.json();
+      setTodo(data);
+      console.log(data);
+    } else {
+      setError("This todo does not exist in our big juicy database");
+    }
+    setLoading(false);
   };
 
   return (
     <div className="App">
-      <div>
-        {showText ? (
-          <>
-            <h1>You will add by {addBy}</h1>
-            <h1>{number}</h1>
-            <button onClick={() => DoSomething(addBy)}>Add</button>
-          </>
-        ) : (
-          <>
-            <h1>Enter a Number</h1>
-            <input
-              type="text"
-              onChange={(e) => setAddBy(Number(e.target.value))}
-            />
-            <button onClick={() => (addBy > 0 ? setShowText(true) : "")}>
-              OK
-            </button>
-          </>
-        )}
-      </div>
+      <input type="text" onChange={(e) => SetId(e.target.value)} />
+      <button onClick={() => fetchToDoById(id)}>fetch</button>
+      {loading && <h1>LOADING !!!</h1>}
+      {error && <p>{error}</p>}
+
+      {todo && <h1>Title : {todo.title} </h1>}
     </div>
   );
 }
